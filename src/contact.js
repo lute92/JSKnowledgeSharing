d93338d0db2b660f013ce06811ab570e1b0a0273
Lute92 <sithukyaw992@gmail.com>
2017-05-12 21:34:37 +0630

    Add new files

--- src/contact.js
+++ src/contact.js
@@ -7,7 +7,7 @@ Contact.prototype = function(){
   var addContact = function(contact){
     this.name = contact.name;
     this.phoneNos = contact.phoneNos.map(function(phNo){
-      return `+95${phNo}`;
+      return "+95 " + phNo;
     });
   }
   return{
